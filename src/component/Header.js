7826b486d5b76a7494dee38b4a46156c86efb9bc
laith-hayajneh <farfesh96@gmail.com>
2021-06-29 16:46:01 +0300

    finish lab01

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import { react } from '@babel/types';
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+        return (
+            <h1> </h1>
+            
+        )
+    }
+}
+
+export default Header;
