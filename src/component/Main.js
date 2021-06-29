763d05e11077961c466508702085f591753ee938
laith-hayajneh <farfesh96@gmail.com>
2021-06-29 20:08:08 +0300

    lab02

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,7 +2,16 @@ import React from 'react';
 import HornedBeasts from './HornedBeasts';
 
 
+
 class Main extends React.Component{
+    
+
+
+
+
+
+
+
     render(){
         return(
             <div>
